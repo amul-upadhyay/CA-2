b331b9451e312b8814c0bbc0d80a2a64b457ea98
amul-upadhyay <l00144562@student.lyit.ie>
2019-04-09 23:16:25 +0100

    Removed "On or near" string from Location Column

--- NI_Crime.R
+++ NI_Crime.R
@@ -1,5 +1,5 @@
 
-# Setting working directory where all crime data files are present.
+## Setting working directory where all crime data files are present.
 
 setwd("~/CA-2/NI Crime Data")
 
@@ -21,14 +21,14 @@ All_Crime_Data <- lapply(filenames,function(i){
   read.csv(i, header=FALSE, skip=4)
 })
 
-All
+All_Crime_Data
 
 # To use the datasets, either to do matching in R,
 # Or to save as its own csv file, 
 # we need to turn it into a dataframe.
 # To do this, we can run the following:
 
-AllNICrimeData <- do.call(rbind.data.frame, All)
+AllNICrimeData <- do.call(rbind.data.frame, All_Crime_Data)
 
 head(AllNICrimeData)
 
@@ -36,7 +36,75 @@ colnames(AllNICrimeData) <- c("Crime ID", "Month", "Reported", "Falls within", "
 
 nrow(AllNICrimeData)
 
+setwd("~/CA-2")
+
 write.csv(AllNICrimeData, file = "AllNICrimeData.csv" )
+
+
+
+#################################################
+#                   (B)                         #
+#################################################
+
+
+typeof(AllNICrimeData)
+
+
+colnames(AllNICrimeData)
+
+asd <- AllNICrimeData[, !(names(AllNICrimeData) %in% c("Crime ID", "Reported", "Falls within", "LSOA code", "LSOA name", "Last outcome category", "Context"))]
+asd
+
+
+str(asd)
+
+
+#################################################
+#                   (C)                         #
+#################################################
+
+table(AllNICrimeData$`Crime type`)
+
+install.packages("plyr")
+library(plyr)
+
+
+AllNICrimeData$'crime type categorized' <- revalue(
+AllNICrimeData$`Crime type`, c("Anti-social behaviour"="Summary Offences", 
+                              "Bicycle theft"="Property Crimes",
+                              "Burglary" = "Property Crimes",
+                              "Criminal damage and arson" = "Property Crimes", 
+                              "Drugs" = "Statutory Crimes",
+                              "Other crime" = "Other", "Other theft" = "Other",
+                              "Possession of weapons" = "Incholate Crime",
+                              "Public order" = "summary offences", "Robbery" = "Property Crimes",
+                              "Shoplifting" = "Property Crimes", 
+                              "Theft from the person" = "Personal Crimes",
+                              "Vehicle crime" = "Statutory Crimes",
+                              "Violence and sexual offences" = "Personal Crimes"))
+
+
+head(AllNICrimeData) 
+
+
+
+#################################################
+#                   (D)                         #
+#################################################
+
+install.packages("tm")
+library(tm)
+
+
+
+
+stopwords("en")
+X <- removeWords(stopwords, AllNICrimeData$Location)
+
+
+AllNICrimeData$Location <- gsub("On or near", "\\1", AllNICrimeData$Location)
+head(AllNICrimeData)
+
 
 
 
